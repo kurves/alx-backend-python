7c500d2d30fa559cd74bd14961592a6f1325f251
root <kurves>
2024-08-06 19:49:05 +0000

    modeul async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module async_generator
+This module contains a coroutine that generates random numbers asynchronously.
+"""
+
+import asyncio
+import random
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Coroutine that loops 10 times, each time asynchronously waits 1 second,
+    then yields a random number between 0 and 10.
+
+    Returns:
+    A generator of random float numbers between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
