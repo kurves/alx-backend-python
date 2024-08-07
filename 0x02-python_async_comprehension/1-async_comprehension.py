e64a1e7f22b0157ce5d0c91cf243184c944f28dd
root <kurves>
2024-08-07 20:25:23 +0000

    fix pycodestyle

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
 Module async_comprehension
-This module contains a coroutine that collects 10 random numbers using an async comprehension.
+This module contains a coroutine that collects 10 random numbers.
 """
 
 import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     """
