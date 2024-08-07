3d7d130173cc30732faa060cc9caef7e35ce8730
root <kurves>
2024-08-07 20:16:39 +0000

    aync comprehesion genetrator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,7 +6,7 @@ This module contains a coroutine that collects 10 random numbers using an async 
 
 import asyncio
 from typing import List
-from async_generator import async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """
@@ -18,5 +18,3 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
-    """Return the 10 random numbers"""
