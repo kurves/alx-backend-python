dd82b1d1834bf458cf2e64c985d2047dd1263079
kurves <kavssimply@gmail.com>
2024-08-08 19:52:45 +0000

    fix pysodestyle formatting

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,13 +8,11 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine that loops 10 times, each time asynchronously waits 1 second,
     then yields a random number between 0 and 10.
-
-    Returns:
-    A generator of random float numbers between 0 and 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)
