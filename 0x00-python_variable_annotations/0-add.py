b1440a346014be0ce0e5c703638e749d5465a4c9
kurves <kavssimply@gmail.com>
2024-08-11 17:12:06 +0000

    python variabl annotatios

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for a simple type-annotated function that adds two floats.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers together and returns the result.
+    Returns:
+    float: The sum of a and b.
+    """
+    return a + b
