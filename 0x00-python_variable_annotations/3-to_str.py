34d57beec58b02fd3d62de6887dd7c9666ebada3
kurves <kavssimply@gmail.com>
2024-08-11 19:51:38 +0000

    python variables annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for a simple type-annotated function that converts a float.
+"""
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+    Returns:
+    str: The string representation of the float.
+    """
+    return str(n)
