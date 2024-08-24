aa4aa15eb52d53cb7fe608978301dfeb26e2e4db
kurvs <kavssimply@gmail.com>
2024-08-24 19:20:48 +0000

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,6 +27,19 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, {'key': 'value'})
 
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """
+        Test that GithubOrgClient._public_repos_url returns the correct value.
+        """
+        mock_org.return_value = {"repos_url": "https://api.github.com/orgs/google/repos"}
+
+        client = GithubOrgClient("google")
+
+        result = client._public_repos_url
+
+        self.assertEqual(result, "https://api.github.com/orgs/google/repos")
+
 
 if __name__ == "__main__":
     unittest.main()
